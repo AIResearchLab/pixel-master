1dbdcc10d14d25580918db768e6627f3a74eb246
Niranjan Shah <niranjanshah474@gmail.com>
2024-04-01 17:39:42 +1100

    new function added to visualize mask to show multi segment masks

--- pages/3_Visualize_Masks.py
+++ pages/3_Visualize_Masks.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image
 import streamlit as st
-
+import matplotlib
 
 def load_and_display_mask_with_pil(mask_file):
     """
@@ -21,7 +21,7 @@ def load_and_display_mask_with_pil(mask_file):
         mask = (mask / mask.max()) * 255.0
     mask = mask.astype(np.uint8)
 
-    # Check the shape and adjust if necessary
+    # Check the shape
     if len(mask.shape) == 3 and mask.shape[2] == 1:
         mask = np.squeeze(mask)  # Remove the singleton dimension
 
@@ -30,12 +30,26 @@ def load_and_display_mask_with_pil(mask_file):
 
     return mask_image
 
+def mask_to_colored_pil(mask, colormap='viridis'):
+    # load the mask, convert it into np format and change dtype to int32
+    mask = np.load(mask, allow_pickle=True).astype(np.int32)
+
+    # Apply colormap
+    normed_data = (mask - np.min(mask)) / (np.max(mask) - np.min(mask))
+
+    mapped_data = matplotlib.colormaps[colormap](normed_data)
+
+    # Convert to PIL Image
+    img_data = (mapped_data[:, :, :3] * 255).astype(np.uint8)
+    img = Image.fromarray(img_data)
+    return img
+
 if __name__=="__main__":
     st.set_page_config(layout="wide", page_title="PixelMaster")
     st.title("Visualize Masks")
 
     mask_uploaded = st.file_uploader("Choose the mask", type=["npy"])
     if mask_uploaded:
-        mask_image_generated = load_and_display_mask_with_pil(mask_uploaded)
+        mask_image_generated = mask_to_colored_pil(mask_uploaded)
 
         st.image(mask_image_generated)
